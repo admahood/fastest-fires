ad386133374949e7e5d9e699deff4cd806898956
‘admahood’ <admahood@gmail.com>
2019-07-23 16:50:33 -0600

    worked on duration figures

--- src/r/average_duration.r
+++ src/r/average_duration.r
@@ -53,7 +53,8 @@ dd_lc <- d %>%
   ungroup()%>%
   filter(lc != "Barren" & 
            lc != "Permanent Snow and Ice" &
-           lc != "Water Bodies") %>%
+           lc != "Water Bodies"&
+           lc != "Permanent Wetlands") %>%
   nest(-lc) %>%
   mutate(fit = map(data, ~ mblm(duration_mean ~ year, data=., repeated=F)),
          results = map(fit, augment)) %>%
@@ -65,7 +66,11 @@ dd_lc_lme <- d %>%
   mutate(fit = map(data, ~ lm(duration ~ year,
                               data = .)),
          results = map(fit, augment)) %>%
-  unnest(results)
+  unnest(results) %>%
+  filter(lc != "Barren" & 
+           lc != "Permanent Snow and Ice" &
+           lc != "Water Bodies" &
+           lc != "Permanent Wetlands") 
 
 write_csv(dd_lc, "data/yearly_duration_lc.csv")
 
@@ -80,7 +85,7 @@ dd$mod3 = predict(mod3, newdata = dd)
 
 
 ts_list <- list()
-table_ <- tibble(ecoregion = NA, slope = NA, p = NA)
+table_ <- tibble(ecoregion = NA, slope = NA, p = NA, min_duration= NA, max_duration = NA)
 for(i in 1:length(unique(d$ecoregion))){
   print(i)
   ddd <- filter(dd, ecoregion == unique(d$ecoregion)[i])
@@ -90,6 +95,8 @@ for(i in 1:length(unique(d$ecoregion))){
   table_[i,1] <- unique(d$ecoregion)[i]
   table_[i,2] <- round(ts_list[[i]]$coefficients[2] %>% as.numeric,3)
   table_[i,3] <- round(ss$coefficients[2,4],5)
+  table_[i,4] <- min(ddd$duration_mean)
+  table_[i,5] <- max(ddd$duration_mean)
 }
 write_csv(table_, "images/ts_table.csv")
 
@@ -102,15 +109,13 @@ pp <- ggplot(dd, aes(x=year, y=duration_mean))+
   geom_line(data = dd_lme, aes(y = .fitted + (.se.fit*1.96), color = ecoregion), lty=3)+
   geom_line(data = dd_lme, aes(y = .fitted - (.se.fit*1.96), color = ecoregion), lty=3)+
   facet_wrap(~ecoregion, scales = "free")+
+  geom_label(data = table_,
+             aes(x=2002, y=max_duration, group = ecoregion, label = paste("p =",round(p,3))),
+             hjust = "left", alpha = 0.5, label.size = 0.25)+
   theme_pubr() +
-  theme(legend.position = "none");pp
-
-cowplot::ggdraw()+
-  cowplot::draw_plot(pp,0,0,1,1)+
-  cowplot::draw_grob(tableGrob(table_, rows = NULL,
-                               theme = ttheme_minimal(base_size =9)),
-                     .6,.07,.2,.2) +
+  theme(legend.position = "none")+
   ggsave("images/ts_lmm.png", width=12, height = 9)
+
 
 ggplot(d, aes(x = year, y=duration, color = ecoregion)) +
   #geom_point() +
@@ -118,18 +123,21 @@ ggplot(d, aes(x = year, y=duration, color = ecoregion)) +
   geom_line(aes(y=predict(mod2)))
 
 # by landcover --------------------------------------------------------
-
+d<-d %>% filter(lc != "Barren" & 
+                    lc != "Permanent Snow and Ice" &
+                    lc != "Water Bodies"&
+                  lc != "Permanent Wetlands")
 
 lmer(duration ~ year + (year|lc), d) -> mod3
 dd_lc$mod3 = predict(mod3, newdata = dd_lc)
 
-lm(duration ~ year*lc, d) -> mod3
+lm(duration ~ year*lc, d ) -> mod3
 dd_lc$mod3 = predict(mod3, newdata = dd_lc)
 
 
 ts_list <- list()
-table_ <- tibble(lc = NA, slope = NA, p = NA)
-for(i in 1:length(unique(d$lc))){
+table_ <- tibble(lc = NA, slope = NA, p = NA, min_duration=NA, max_duration = NA)
+for(i in 1:(length(unique(d$lc)))){
   print(i)
   ddd <- filter(dd_lc, lc == unique(d$lc)[i])
   ts_list[[i]] <- mblm(duration_mean ~ year, ddd, repeated =F)
@@ -138,8 +146,14 @@ for(i in 1:length(unique(d$lc))){
   table_[i,1] <- unique(d$lc)[i]
   table_[i,2] <- round(ts_list[[i]]$coefficients[2] %>% as.numeric,3)
   table_[i,3] <- round(ss$coefficients[2,4],5)
+  table_[i,4] <- min(ddd$duration_mean)
+  table_[i,5] <- max(ddd$duration_mean)
 }
+
+
 write_csv(table_, "images/ts_lc_table.csv")
+
+
 
 pp <- ggplot(dd_lc, aes(x=year, y=duration_mean))+
   geom_point() +
@@ -150,14 +164,13 @@ pp <- ggplot(dd_lc, aes(x=year, y=duration_mean))+
   geom_line(data = dd_lc_lme, aes(y = .fitted + (.se.fit*1.96), color = lc), lty=3)+
   geom_line(data = dd_lc_lme, aes(y = .fitted - (.se.fit*1.96), color = lc), lty=3)+
   facet_wrap(~lc, scales = "free")+
+  geom_label(data = table_,
+             aes(x=2002, y=max_duration, group = lc, label = paste("p =",round(p,3))),
+             hjust = "left", alpha = 0.5, label.size = 0.25)+
   theme_pubr() +
   theme(legend.position = "none")+
   ggsave("images/ts_lm_lc.png", width=10, height = 10);pp
 
-cowplot::ggdraw()+
-  cowplot::draw_grob(tableGrob(table_, rows = NULL,
-                               theme = ttheme_minimal())) +
-  ggsave("images/ts_lmm_table.png")
 # gmod<-gls(duration ~ year, d[d$lc == "Barren",], correlation = corAR1(form = ~1|year))
 # 
 # ggplot(d[d$lc == "Barren",], aes(x = year, y=duration)) +
